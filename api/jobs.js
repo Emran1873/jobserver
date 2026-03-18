66978e4195b2132a6cc3db45b14fb71f125b9b0e
Emran <91852308+Emran1873@users.noreply.github.com>
2026-03-18 10:24:58 +0300

    Update Telegram channel username in jobs.js

--- api/jobs.js
+++ api/jobs.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const CHANNEL = "channel_username_here";
+    const CHANNEL = "freelance_ethio";
 
     const url = https://t.me/s/${CHANNEL}?embed=1&mode=tme;
 
